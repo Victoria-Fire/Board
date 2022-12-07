30ac11ac31132642e4bfab53e447d607c05fbe83
Victoria-Fire <vik.fire@yandex.ru>
2022-12-07 23:24:31 +0300

    Add files

---
+++ app.js
@@ -0,0 +1,27 @@
+const board = document.querySelector('#board')
+const SQUARES_NUMBER = 500;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+    square.addEventListener('mouseover', setColor)
+    square.addEventListener('mouseleave', removeColor)
+    board.append(square)
+}
+
+function setColor(event) {
+    const element = event.target
+    const color = 'rgb(' + getRandomColor(0,255) + ', ' + getRandomColor(0,255) + ', ' + getRandomColor(0,255) + ')';
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(event) {
+    const element = event.target
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
